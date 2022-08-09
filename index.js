47a5d01ea8c0275243e02f7757a18430b4e5865d
emericgautier <emericgtr@gmail.com>
2022-08-09 21:31:57 +0200

    logic, recursive function

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 let array = ["développeur", "photographe", "créatif"];
+let wordIndex = 0;
+let letterIndex = 0;
+// target.textContent = array[0];
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+
+  letter.textContent = array[wordIndex][letterIndex];
+
+  setTimeout(() => {
+    letter.remove();
+  }, 2800);
+};
+
+// setInterval(createLetter, 200);
+
+// setInterval(() => {
+//   letterIndex++;
+//   createLetter();
+// }, 200);
+
+// setTimeout(() => {
+//   letterIndex++;
+//   createLetter();
+// }, 200);
+
+const loop = () => {
+  setTimeout(() => {
+    if (wordIndex >= array.length) {
+      wordIndex = 0;
+      letterIndex = 0;
+      loop();
+    } else if (letterIndex < array[wordIndex].length) {
+      createLetter();
+      letterIndex++;
+      loop();
+    } else {
+      wordIndex++;
+      letterIndex = 0;
+      setTimeout(() => {
+        loop();
+      }, 2800);
+    }
+  }, 60);
+};
+
+loop();
